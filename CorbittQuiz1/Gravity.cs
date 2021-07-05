bbc965363fc6e7a718c79be094ad768ede6bf48b
DESKTOP-SVECMJF\BC <scorbitt@comcast.net>
2021-07-05 13:59:32 -0600

    Add project files.

---
+++ CorbittQuiz1/Gravity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorbittQuiz1
+{
+    class Gravity
+    {
+        private const double GRAV = 6.67408e-11;  //grav constant
+        private double mass1;
+        private double mass2;
+        private double distance = -1.0; //init to -1 to stop div by zero error in calc prior to all values set        
+
+        public double Force { get; private set; }
+        public double Mass1
+        {
+            get
+            {
+                return mass1;
+            }
+            set
+            {
+                mass1 = value;
+                calcForce();
+            }
+        }
+        public double Mass2
+
+        {
+            get
+            {
+                return mass2;
+            }
+            set
+            {
+                mass2 = value;
+                calcForce();
+            }
+        }
+        public double Distance
+        {
+            get
+            {
+                return distance;
+            }
+            set
+            {
+                distance = value;
+                calcForce();
+            }
+        }
+
+        public void calcForce()
+        {
+            Force = GRAV * Mass1 * Mass2 / (Distance * Distance);
+        }
+
+    }
+}
+
---
+++ CorbittQuiz1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CorbittQuiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string another;
+
+            do
+            {
+                Gravity grav = new Gravity();
+
+                //get Mass1 from user
+                Console.WriteLine("Please input the mass of the first object in kg: ");
+                grav.Mass1 = Convert.ToDouble(Console.ReadLine());
+
+                //get Mass2 from user
+                Console.WriteLine("Please input the mass of the second object in kg: ");
+                grav.Mass2 = Convert.ToDouble(Console.ReadLine());
+
+                //get distance from user
+                Console.WriteLine("Please input the distance between the objects in meters: ");
+                grav.Distance = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("\n\nThe gravitational force between your two objects is " +
+                  grav.Force + " newtons.");
+
+                Console.WriteLine("Would you like to calculate another gravitational force? (enter yes to continue): ");
+                another = Console.ReadLine();
+            }
+            while (string.Equals(another.ToLower(), "yes"));
+        }
+    }
+}
